3ad6cb0886bfec86974d9f2eaafd55db3bedb441
Julianne Yang <julianneyangcollege@gmail.com>
2023-07-17 09:44:47 -0700

    beta diversity

--- Q22_Rproj/Q22_RS_Jensen_Shannon.R
+++ Q22_Rproj/Q22_RS_Jensen_Shannon.R
@@ -28,51 +28,25 @@ ileum <- ileum_meta$SampleID
 ileum_counts <- counts %>% select(all_of(ileum))
 
 # Mucosal Colon
-muccol_meta <- metadata %>% filter(Subset=="Mucosal_Colon", SampleID %in% names(counts))
-row.names(muccol_meta) <- muccol_meta$SampleID
-muccol <- muccol_meta$SampleID
-muccol_counts <- counts %>% select(all_of(muccol))
-
-# Luminal Colon no HET 
-nohet_lt_ileum_meta <- metadata %>%
-  filter(Genotype!="HET")%>%
-  filter(Subset=="Luminal_Colon", SampleID %in% names(counts))
-row.names(nohet_lt_ileum_meta) <- nohet_lt_ileum_meta$SampleID
-ileum <- nohet_lt_ileum_meta$SampleID
-nohet_ileum_counts <- counts %>% select(all_of(ileum))
-
-# Mucosal Colon no HET
-nohet_lt_muccol_meta <- metadata %>% 
-  filter(Genotype!="HET")%>%
-  filter(Subset=="Mucosal_Colon", SampleID %in% names(counts))
-row.names(nohet_lt_muccol_meta) <- nohet_lt_muccol_meta$SampleID
-muccol <- nohet_lt_muccol_meta$SampleID
-nohet_muccol_counts <- counts %>% select(all_of(muccol))
+colon_meta <- metadata %>% filter(Site=="COL", SampleID %in% names(counts))
+row.names(colon_meta) <- colon_meta$SampleID
+colon <- colon_meta$SampleID
+colon_counts <- counts %>% select(all_of(colon))
 
 ## Prevalence filter datasets -- 
 # Ileum
 0.15*28 #4 samples
 ileum_counts <- prevalence_filter(ileum_counts,4)
 
-# Mucosal Colon 
-0.15*89
-muccol_counts <- prevalence_filter(muccol_counts,13)
-
-# Luminal Colon - no HET 
-0.15*42 #6 samples
-nohet_lt_lumcol_counts <- prevalence_filter(nohet_lumcol_counts,6)
-
-# Mucosal Colon - no HET 
-0.15*41 #6 samples
-nohet_lt_muccol_counts <- prevalence_filter(nohet_muccol_counts,6)
+#  Colon 
+0.15*28 #4 samples
+colon_counts <- prevalence_filter(colon_counts,4)
 
 ## Calculate RS Jensen Shannon distance matrix -- 
 
 
-muccol.dist <- calculate_rsjensen(muccol_counts)
+colon.dist <- calculate_rsjensen(colon_counts)
 ileum.dist <- calculate_rsjensen(ileum_counts)
-lt_ileum.dist <- calculate_rsjensen(nohet_lt_ileum_counts)
-lt_muccol.dist <- calculate_rsjensen(nohet_lt_muccol_counts)
 
 ## Principal Coordinates Analysis -- 
 
@@ -99,36 +73,22 @@ ileum_pcoa_Litter <- generate_pcoA_plots(distance_matrix=ileum.dist,
                                   wa_scores_filepath = "Q22_Microbiome/beta_diversity/ileum_Top_Taxa_RSJ_PcoA.csv")
 ileum_pcoa_Litter
 
-slt_mc_pcoa <- generate_pcoA_plots(distance_matrix=muccol.dist,
-                                     counts = muccol_counts,
-                                     metadata = muccol_meta,
+cols <- c("WT"="black", "KO"="red")
+colon_pcoa <- generate_pcoA_plots(distance_matrix=colon.dist,
+                                     counts = colon_counts,
+                                     metadata = colon_meta,
                                      title="Long Term - Mucosal Colon RS Jensen",
-                                     colorvariable = Genotype,
+                                     colorvariable = Q22,
                                      colorvector = cols,
-                                     wa_scores_filepath = "Long_Term/MucCol_Top_Taxa_PcoA.csv")
-nohet_slt_lc_pcoa <- generate_pcoA_plots(distance_matrix=lt_ileum.dist,
-                                   counts = nohet_ileum_counts,
-                                   metadata = nohet_lt_ileum_meta,
-                                   title="Long Term - Luminal Colon RS Jensen",
-                                   colorvariable = Genotype,
-                                   colorvector = cols,
-                                   wa_scores_filepath = "Long_Term/nohet_ileum_Top_Taxa_PcoA.csv")
-
-nohet_slt_mc_pcoa <- generate_pcoA_plots(distance_matrix=lt_muccol.dist,
-                                   counts = nohet_muccol_counts,
-                                   metadata = nohet_lt_muccol_meta,
-                                   title="Long Term - Mucosal Colon RS Jensen",
-                                   colorvariable = Genotype,
-                                   colorvector = cols,
-                                   wa_scores_filepath = "Long_Term/nohet_MucCol_Top_Taxa_PcoA.csv")
-
+                                     wa_scores_filepath = "Q22_Microbiome/beta_diversity/colon_Top_Taxa_PcoA.csv")
+colon_pcoa 
 plot_grid(slt_lc_pcoa,slt_mc_pcoa, labels=c("C","D"),label_size = 20)
 plot_grid(nohet_slt_lc_pcoa,nohet_slt_mc_pcoa, labels=c("C","D"),label_size = 20)
 
 
 ## PERMANOVA
 
-# Luminal Colon 
+# Ileum
 data.dist<-ileum.dist
 metadata <- ileum_meta
 
@@ -142,8 +102,8 @@ data.adonis=adonis(data.dist ~ Litter+ Sex + Q22, data=metadata, permutations=10
 data.adonis$aov.tab
 
 # Mucosal Colon
-data.dist<-muccol.dist
-metadata <- muccol_meta
+data.dist<-colon.dist
+metadata <- colon_meta
 
 target <- row.names(data.dist)
 metadata = metadata[match(target, row.names(metadata)),]
@@ -151,7 +111,7 @@ target == row.names(metadata)
 data.dist <- as.dist(as(data.dist, "matrix"))
 
 set.seed(11)
-data.adonis=adonis(data.dist ~ Sex + Site + Genotype, data=metadata, permutations=10000)
+data.adonis=adonis(data.dist ~ Litter+ Sex + Q22, data=metadata, permutations=10000)
 data.adonis$aov.tab
 
 # Luminal Colon -- no HET 
